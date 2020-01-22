243a52d5b4fa2c4dd896862325a1e0837ddb8432
Aditya Sabbella <Aditya_Sabbella@epam.com>
2020-01-22 18:27:26 +0530

    adding comments-example

---
+++ C#/Comments-Example/Calculator.cs
@@ -0,0 +1,167 @@
+﻿namespace ConsoleApp
+{
+    /*
+    The main Calculator class
+    Contains all methods for performing basic math functions
+*/
+    /// <summary>
+    /// The main <c>Calculator</c> class.
+    /// Contains all methods for performing basic math functions.
+    /// <list type="bullet">
+    /// <item>
+    /// <term>Add</term>
+    /// <description>Addition Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Subtract</term>
+    /// <description>Subtraction Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Multiply</term>
+    /// <description>Multiplication Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Divide</term>
+    /// <description>Division Operation</description>
+    /// </item>
+    /// </list>
+    /// </summary>
+    /// <remarks>
+    /// <para> This class can add, subtract, multiply and divide. </para>
+    /// </remarks>
+    public class Calculator
+    {
+        // Adds two integers and returns the result
+        /// <summary>
+        /// Adds two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The sum of two integers.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// int c = Calculator.Add(4, 5);
+        /// if (c > 10)
+        /// {
+        ///     Console.WriteLine(c);
+        /// }
+        /// </code>
+        /// </example>
+        /// <exception cref="System.OverflowException">Thrown when one parameter is max 
+        /// and the other is greater than 0.</exception>
+        /// <seealso cref="Calculator.Subtract(int, int)"/>
+        /// <seealso cref="Calculator.Multiply(int, int)"/>
+        /// <seealso cref="Calculator.Divide(int, int)"/>
+        /// <param name="a">An integer.</param>
+        /// <param name="b">An integer.</param>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Subtracts an integer from another and returns the result
+        /// <summary>
+        /// Subtracts <paramref name="b"/> from <paramref name="a"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The difference between two integers.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// int c = Calculator.Subtract(4, 5);
+        /// if (c > 1)
+        /// {
+        ///     Console.WriteLine(c);
+        /// }
+        /// </code>
+        /// </example>
+        /// <seealso cref="Calculator.Add(int, int)"/>
+        /// <seealso cref="Calculator.Multiply(int, int)"/>
+        /// <seealso cref="Calculator.Divide(int, int)"/>
+        /// <param name="a">An integer.</param>
+        /// <param name="b">An integer.</param>
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Multiplies two integers and returns the result
+        /// <summary>
+        /// Multiplies two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The product of two integers.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// int c = Calculator.Multiply(4, 5);
+        /// if (c > 100)
+        /// {
+        ///     Console.WriteLine(c);
+        /// }
+        /// </code>
+        /// </example>
+        /// <seealso cref="Calculator.Add(int, int)"/>
+        /// <seealso cref="Calculator.Subtract(int, int)"/>
+        /// <seealso cref="Calculator.Divide(int, int)"/>
+        /// <param name="a">An integer.</param>
+        /// <param name="b">An integer.</param>
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        // Divides an integer by another and returns the result
+        /// <summary>
+        /// Divides an integer <paramref name="a"/> by another integer <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The quotient of two integers.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// int c = Calculator.Divide(4, 5);
+        /// if (c > 1)
+        /// {
+        ///     Console.WriteLine(c);
+        /// }
+        /// </code>
+        /// </example>
+        /// <exception cref="System.DivideByZeroException">Thrown when <paramref name="b"/> is equal to 0.</exception>
+        /// See <see cref="Calculator.Divide(double, double)"/> to divide doubles.
+        /// <seealso cref="Calculator.Add(int, int)"/>
+        /// <seealso cref="Calculator.Subtract(int, int)"/>
+        /// <seealso cref="Calculator.Multiply(int, int)"/>
+        /// <param name="a">An integer dividend.</param>
+        /// <param name="b">An integer divisor.</param>
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        // Divides a double by another and returns the result
+        /// <summary>
+        /// Divides a double <paramref name="a"/> by another double <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The quotient of two doubles.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// double c = Calculator.Divide(4.5, 5.4);
+        /// if (c > 1.0)
+        /// {
+        ///     Console.WriteLine(c);
+        /// }
+        /// </code>
+        /// </example>
+        /// <exception cref="System.DivideByZeroException">Thrown when <paramref name="b"/> is equal to 0.</exception>
+        /// See <see cref="Calculator.Divide(int, int)"/> to divide integers.
+        /// <param name="a">A double precision dividend.</param>
+        /// <param name="b">A double precision divisor.</param>
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ C#/Comments-Example/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The Main function.
+        /// </summary>
+        /// <returns>
+        /// Void
+        /// </returns>
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+
+            Console.WriteLine("Enter two numbers:");
+
+            ///<value>
+            ///the <c>number</c> varialbe is used to store the first operand of user input
+            /// </value>
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            ///<value>
+            ///the <c>number</c> varialbe is used to store the second operand of user input
+            /// </value>
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Division(in decimals)\n");
+
+            Console.WriteLine("\nChoose any one:");
+            
+            ///<value>
+            ///the <c>option</c> varialbe is used to store the value of selected options from user
+            /// </value>
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Addition of two numbers:");
+                    Console.WriteLine(Calculator.Add(number1,number2));
+                    break;
+                case 2:
+                    Console.WriteLine("Subtraction of two numbers:");
+                    Console.WriteLine(Calculator.Subtract(number1, number2));
+                    break;
+                case 3:
+                    Console.WriteLine("Multiplicaion of two numbers:");
+                    Console.WriteLine(Calculator.Multiply(number1, number2));
+                    break;
+                case 4:
+                    Console.WriteLine("Division of two numbers:");
+                    Console.WriteLine(Calculator.Divide(number1, number2));
+                    break;
+                case 5:
+                    Console.WriteLine("Division of two numbers(in decimals):");
+                    Console.WriteLine(Calculator.Divide((double)number1, (double)number2));
+                    break;
+                default:
+                    Console.WriteLine("Wrong Option Selected");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
