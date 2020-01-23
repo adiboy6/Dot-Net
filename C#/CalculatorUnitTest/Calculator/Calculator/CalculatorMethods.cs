ed07846f39fe9d941fef49db3e791d6940b44a71
Aditya Sabbella <Aditya_Sabbella@epam.com>
2020-01-23 18:21:48 +0530

    adding Claculator Unit Test example

---
+++ C#/CalculatorUnitTest/Calculator/Calculator/CalculatorMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalculatorMethods
+    {
+        public static int add(int a,int b)
+        {
+            return a + b;
+        }
+
+        public static int subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static double divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ C#/CalculatorUnitTest/Calculator/Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int addition_ans = CalculatorMethods.add(Int32.MinValue, Int32.MinValue);
+
+            //Sample Run
+            Console.WriteLine(addition_ans);
+
+
+            int subtraction_ans = CalculatorMethods.subtract(Int32.MaxValue, Int32.MaxValue);
+            
+            //Sample Run
+            Console.WriteLine(subtraction_ans);
+
+
+            double division_ans = CalculatorMethods.divide(1, 2);
+            
+            //Sample Run
+            Console.WriteLine(division_ans);
+        }
+    }
+}
