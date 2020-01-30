650d8b2a45a8c26a5c9d6deb839c38c4720f3b11
Aditya Sabbella <aditya_sabbella@epam.com>
2020-01-30 08:37:23 +0530

    adding Collection.Generic project

---
+++ C#/Collection.Generic/HashSetExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 2, 3, 4, 6, 7, 8 };
+
+            Console.WriteLine("Size of array " + array.Length);
+            
+            //duplicates are removed and added in the set
+            HashSet<int> hashSet = new HashSet<int>(array);
+
+            Console.WriteLine("Unique elements of array in a set");
+            foreach (int value in hashSet)
+                Console.Write(value + " ");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hash Code is " + hashSet.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Collection.Generic/ListExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> int_list = new List<int>();
+
+            int_list.Add(1);
+            int_list.Add(4);
+            int_list.Add(3);
+            int_list.Add(5);
+            int_list.Add(2);
+
+            Console.WriteLine("Size of list is {0}", int_list.Count);
+
+            //Parses out the list
+            Console.WriteLine("Values of list:");
+            foreach (int element in int_list)
+                Console.Write(element + " ");
+
+            Console.WriteLine();
+
+            //Computes the average of elements
+            Console.WriteLine("Average is " + int_list.Average());
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
