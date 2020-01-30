88fc150cbfc86f0681d985eb6c5d226ac56789e4
Aditya Sabbella <aditya_sabbella@epam.com>
2020-01-30 11:23:25 +0530

    QueueExample project added

---
+++ C#/Collection.Generic/QueueExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<String> queue = new Queue<string>();
+
+            queue.Enqueue("A");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+            queue.Enqueue("d");
+            queue.Enqueue("e");
+
+            //First element is Queue
+            Console.WriteLine("First element is " + queue.Peek());
+
+            //Tries wheter the queue still has any elements or not
+            while (queue.TryDequeue(out string value))
+                Console.WriteLine(value);
+        }
+    }
+}
