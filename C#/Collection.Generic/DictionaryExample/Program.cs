d05e0d266b7f5abac9a7c40828066574b3b8e986
Aditya Sabbella <aditya_sabbella@epam.com>
2020-01-30 10:49:57 +0530

    DictionaryExample and LinkedListExample projects are added

---
+++ C#/Collection.Generic/DictionaryExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<object, string> studentDetails = new Dictionary<object, string>();
+
+            studentDetails.Add(1, "Joy");
+            studentDetails.Add(2, "Asta");
+            studentDetails.Add(3, "Yuno");
+            studentDetails.Add(4, "Magma");
+
+            try
+            {
+                studentDetails.Add(null, "some student");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+
+
+            Console.WriteLine("\nShowing the Student Details");
+            foreach(var key in studentDetails.Keys)
+            {
+                Console.WriteLine("ID:" + key + "\nNAME:" + studentDetails[key] + "\n");
+            }
+
+            //Throws an exception : ArgumentException since key already exists
+            //studentDetails.Add(1, "Jack");
+
+            Console.WriteLine("\nShowing the Names of Students");
+            foreach (var value in studentDetails.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine();
+
+            //Check if a key is present or not
+            if (studentDetails.ContainsKey(3))
+                Console.WriteLine("ID is present");
+            else
+                Console.WriteLine("ID is not present");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Collection.Generic/LinkedListExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //works as a double linked list
+            LinkedList<int> int_list = new LinkedList<int>();
+
+            int_list.AddFirst(1);
+            int_list.AddFirst(2);
+            int_list.AddFirst(3);
+            int_list.AddFirst(4);
+            int_list.AddFirst(5);
+
+            //works as Add method as in List or ArrayList
+            int_list.AddLast(0);
+            int_list.AddLast(-1);
+
+            Console.WriteLine("Size of  the list " + int_list.Count);
+
+            Console.WriteLine("Showing the List:");
+            foreach (var value in int_list)
+                Console.Write(value + " ");
+            Console.WriteLine();
+            
+            //Removes the First node
+            int_list.RemoveFirst();
+
+            //Removes the Last node
+            int_list.RemoveLast();
+
+            Console.WriteLine("Showing the List after removing first and last node:");
+            foreach (var value in int_list)
+                Console.Write(value + " ");
+            Console.WriteLine();
+
+            LinkedListNode<int> first_node = int_list.First;
+
+            Console.WriteLine("First value of the list is " + first_node.Value);
+
+            Console.WriteLine("Second value of the list is " + first_node.Next.Value);
+
+            Console.ReadKey();
+        }
+    }
+}
