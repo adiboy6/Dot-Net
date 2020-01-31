b147d853dfbca5db6475c67f70f6cf1ea1c8b9fc
Aditya Sabbella <aditya_sabbella@epam.com>
2020-01-31 12:31:57 +0530

    BlockingCollectionExample project added

---
+++ C#/Collection.Concurrent/BlockingCollectionExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace BlockingCollectionExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (BlockingCollection<int> blockingQueue = new BlockingCollection<int>())
+            {
+                //PRODUCER
+                Task producer = Task.Run( ()=>
+                {
+                    for (int num = 0; num < 5; num++)
+                        blockingQueue.Add(num);
+                    blockingQueue.CompleteAdding();
+                });
+
+                //CONSUMER
+                Task consumer = Task.Run(() =>
+                {
+                    while (blockingQueue.TryTake(out int value))
+                        Console.WriteLine("Consumed : " + value);
+                });
+
+                //only works if it's aynchronous (uses async keyword)
+                await Task.WhenAll(producer, consumer);
+
+            }
+
+        }
+    }
+}
