ddaf77f10d2830a7e8f4db504e292fca8a27e4d0
Aditya Sabbella <aditya_sabbella@epam.com>
2020-01-31 14:35:10 +0530

    ReadOnlyCollectionExample project added

---
+++ C#/Collection.ObjectModel/Collection.ObjectModel/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Collection.ObjectModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C#/Collection.ObjectModel/ReadOnlyCollectionExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ReadOnlyCollectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+            list.Add("D");
+            list.Add("E");
+
+            //A collection is passed to ReadOnly. This collection(ReadOnlyCollection) value cannot be modified. 
+            ReadOnlyCollection<string> readonlyList = new ReadOnlyCollection<string>(list);
+
+            foreach(string value in readonlyList)
+            {
+                Console.WriteLine(value);
+            }
+
+            //Not possible since the value cannot be changed
+            //readonlyList[0] = "adding somthing";
+        }
+    }
+}
