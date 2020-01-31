09d85e0075a266bf39d04fb758503bd2f5fa9c49
Aditya Sabbella <aditya_sabbella@epam.com>
2020-01-31 12:15:49 +0530

    adding ConcurrentStack and ConcurrentQueue Example

---
+++ C#/Collection.Concurrent/ConcurrentQueueExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentQueueExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+
+            //Producer
+            for (int num = 0; num < 100; num++)
+                queue.Enqueue(num);
+
+            if (queue.TryPeek(out int result))
+                Console.WriteLine("Topmost elment is {0}", result);
+
+            //Consumer
+            Action consume = delegate ()
+            {
+                while (queue.TryDequeue(out int consumed))
+                    Console.WriteLine("Consumed : " + consumed);
+            };
+
+            //Similar to List<Task> and Task.WaitAll combined
+            Parallel.Invoke(consume, consume);
+
+            if (queue.IsEmpty)
+                Console.WriteLine("Queue is empty. Tasks executed sucessfully.");
+            else
+                Console.WriteLine("Queue is not empty. Tasks executed unsucessfully.");
+        }
+    }
+}
---
+++ C#/Collection.Concurrent/ConcurrentStackExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace ConcurrentStackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int items = 100;
+
+            ConcurrentStack<int> stack = new ConcurrentStack<int>();
+
+            List<Task> tasks = new List<Task>();
+
+            Action produce_and_consume = delegate ()
+            {
+                Console.WriteLine($"Task started on {Task.CurrentId}");
+
+                int item;
+                for (int i = 0; i < items; i++)
+                    stack.Push(i);
+                for (int i = 0; i < items; i++)
+                    stack.TryPop(out item);
+
+                Console.WriteLine($"Task ended on {Task.CurrentId}");
+            };
+
+            for (int num = 0; num < 5; num++)
+            {
+                tasks.Add(Task.Run(produce_and_consume));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            if (stack.IsEmpty)
+                Console.WriteLine("Stack is empty");
+
+        }
+    }
+}
