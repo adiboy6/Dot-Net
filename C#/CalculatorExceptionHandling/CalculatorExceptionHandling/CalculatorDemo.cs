b0d8544a8a14430623dd32b57d3f585921d665e8
Aditya Sabbella <aditya_sabbella@epam.com>
2020-01-31 17:57:58 +0530

    Calculator Exception project added

---
+++ C#/CalculatorExceptionHandling/CalculatorExceptionHandling/CalculatorDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CalculatorExceptionHandling
+{
+    class CalculatorDemo
+    {
+        static void Main(string[] args)
+        {
+            CalculatorMethods calculatorMethods = new CalculatorMethods();
+
+            try
+            {
+                int ans = calculatorMethods.add(1,2);
+
+                Console.WriteLine("Addition of two numbers is " + ans);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                int ans = calculatorMethods.add(int.MaxValue,int.MinValue);
+
+                Console.WriteLine("Addition of two numbers is " + ans);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            try
+            {
+                int ans = calculatorMethods.add(int.MaxValue, int.MaxValue);
+
+                Console.WriteLine("Addition of two numbers is " + ans);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ C#/CalculatorExceptionHandling/CalculatorExceptionHandling/CalculatorMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorExceptionHandling
+{
+    class CalculatorMethods
+    {
+        public int add(int a, int b)
+        {
+            if ((a == int.MaxValue && b > 0) || (b == int.MaxValue && a > 0))
+                throw new OverflowException("Addition Overflow");
+            return a + b;
+        }
+
+        public int subtract(int a, int b)
+        {
+            if ((a == int.MinValue && b < 0) || (b == int.MinValue && a < 0))
+                throw new OverflowException("Subtraction Overflow");
+            return a - b;
+        }
+        public int multiply(int a, int b)
+        {
+            if ((a == int.MaxValue && b > 0) || (b == int.MaxValue && a > 0))
+                throw new OverflowException("Multiplication Overflow");
+            return a * b;
+        }
+        public double divide(double a, double b)
+        {
+            if (a == double.MaxValue || a == double.MinValue || b == double.MaxValue || b == double.MinValue)
+                throw new OverflowException("Division Overflow");
+            if((a != 0 && b==0) || (a == 0 && b != 0))
+                throw new 
+            if (b == 0.0f)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
