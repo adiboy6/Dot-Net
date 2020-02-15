ed8a33bd9a6602bdf642ca31fc9e53fc58918837
adiboy6 <aditya.reddy.97@gmail.com>
2020-02-15 21:07:39 +0530

    added a view for tokefield

--- C#/MVC Form/MVC Form/CustomHTMLHelper/TokenfieldHelper.cs
+++ C#/MVC Form/MVC Form/CustomHTMLHelper/TokenfieldHelper.cs
@@ -12,9 +12,14 @@ namespace MVC_Form.CustomHTMLHelper
         public static MvcHtmlString Tokenfield(this HtmlHelper htmlHelper, string setTokens)
         {
             StringBuilder script = new StringBuilder();
+            
             script.AppendLine(@"<script>");
-            script.AppendLine("$(\"#" + setTokens + "\").tokenfield();");
-            script.Append(@"</script>");
+            script.AppendLine("$(\'#" + setTokens + "\').tokenfield({" +
+                "autocomplete: {" +
+                "source: ['red', 'blue', 'green', 'yellow', 'violet', 'brown', 'purple', 'black', 'white'],"+
+                "delay: 100},"+
+                "showAutocompleteOnFocus: true})");
+            script.AppendLine(@"</script>");
             return new MvcHtmlString(script.ToString());
         }
     }
